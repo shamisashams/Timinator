b5803b5a0a8a913eba730262e61fa3b4e9f7c64e
Shammy <76426876+shamisashams@users.noreply.github.com>
2025-10-23 09:38:20 +0200

    add course box filled

--- src/components/AddCourse.jsx
+++ src/components/AddCourse.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const AddCourse = ({ active }) => {
-  return <div className={`${active ? "block" : "hidden"}`}>AddCourse</div>;
+  return (
+    <div className={`${active ? "block" : "hidden"}`}>
+      <div className="bg-gradient border-3 border-transparent max-w-3xl rounded-2xl">
+        <div className="bg-white p-10  rounded-2xl">
+          <div className="text-center">
+            <h2>Add New Course</h2>
+            <div>Create a new course to organize your study schedule</div>
+          </div>
+          <label>Course Topic</label>
+          <input
+            type="text"
+            placeholder="eg. Advanced Mathematics, Web Development"
+          />
+          <label>Project</label>
+          <input
+            type="text"
+            placeholder="eg. Final Exam Preparation, Portfolio Website"
+          />
+          <label>Deadline</label>
+          <input type="date" placeholder="" />
+          <button className="bg-blue p-5 w-full">Add Course</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddCourse;
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const TaskList = ({ active }) => {
-  return <div className={`${active ? "block" : "hidden"}`}>TaskList</div>;
+  return (
+    <div className={`${active ? "block" : "hidden"}`}>
+      TaskList <br />
+      <div className="bg-gradient border-3 border-transparent max-w-3xl rounded-2xl">
+        <div className="bg-white p-10  rounded-2xl">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+          asperiores hic, consectetur minus totam architecto quis iusto, eos
+          voluptatum tempore adipisci, doloribus error aspernatur iure. Aperiam
+          eligendi nulla quisquam animi!
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TaskList;
